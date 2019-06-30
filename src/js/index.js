83940d67f02afd3f5a44805303e43fe58d6427f0
three4c <three4c@gmail.com>
2019-06-30 17:17:06 +0900

    mdの記述からプレビューまで可

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+import { h, app } from "hyperapp";
+import marked from 'marked';
+import "../sass/style.sass";
+import hljs from 'highlightjs';
+import 'highlight.js/styles/monokai.css'
+
+marked.setOptions({
+    highlight: function (code, lang) {
+        return hljs.highlightAuto(code, lang).value
+    }
+})
+
+const state = {
+    'output': ''
+}
+
+const actions = {
+    setOutput: (input) => state => ({ output: marked(input) })
+}
+
+const view = (state, actions) => (
+    <main>
+        <textarea id='editor' oninput={e => actions.setOutput(document.getElementById('editor').value)} />
+        <div id='preview' innerHTML={state.output}></div>
+    </main>
+)
+
+export const main = app(state, actions, view, document.body)
